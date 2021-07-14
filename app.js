5a5dc0c2a3edeb17120abfcf8c251ec16c7a7ef8
Alice-Retz <alicegretz@gmail.com>
2021-07-13 19:15:18 -0500

    Troubleshooting testing

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-import pokes from './data/pokemon.js';
-// import { findById, getPoke } from './storage-utils.js';
-
+import pokemon from './data/pokemon.js';
 
 const poke1Radio = document.getElementById('poke1-radio');
 const poke1Image = document.getElementById('poke1-image');
+
 const poke2Radio = document.getElementById('poke2-radio');
 const poke2Image = document.getElementById('poke2-image');
+
 const poke3Radio = document.getElementById('poke3-radio');
 const poke3Image = document.getElementById('poke3-image');
+
 const capturePokemon = document.getElementById('submit');
 
+
+
 let totalPlays = 0;
-function encounterPokemon(){
+
+export function encounterPokemon(pokemon){
     totalPlays++;
-    let randNum1 = Math.ceil(Math.random()) * pokes.length;
-    let randNum2 = Math.ceil(Math.random()) * pokes.length;
-    let randNum3 = Math.ceil(Math.random()) * pokes.length;
+
+    let randNum1 = Math.floor(Math.random() * pokemon.length);
+    let randNum2 = Math.floor(Math.random() * pokemon.length);
+    let randNum3 = Math.floor(Math.random() * pokemon.length);
 
     while (randNum1 === randNum2 ||
       randNum1 === randNum3 ||
       randNum2 === randNum3) {
-        let randNum1 = Math.ceil(Math.random()) * pokes.length;
-        let randNum2 = Math.ceil(Math.random()) * pokes.length;
-        let randNum3 = Math.ceil(Math.random()) * pokes.length;
-        //console.log('randNum1');
+        randNum1 = Math.floor(Math.random() * pokemon.length);
+        randNum2 = Math.floor(Math.random() * pokemon.length);
+        randNum3 = Math.floor(Math.random() * pokemon.length);
     }    
-    let poke1 = pokes[randNum1];
-    let poke2 = pokes[randNum2];
-    let poke3 = pokes[randNum3];
 
-    poke1Radio.value = poke1.id;
+    console.log(randNum1);
+    console.log(pokemon[randNum1]);
+
+    let poke1 = pokemon[randNum1];
+    console.log(randNum2);
+    let poke2 = pokemon[randNum2];
+    console.log(randNum3);
+    let poke3 = pokemon[randNum3];
+    
+    poke1Radio.value = poke1.pokemon;
+    console.log(poke1Radio.value);
     poke1Radio.checked = false;
-    poke1Image.src = `./assets/${poke1.image}`;
+    poke1Image.src = poke1.url_image;
     
-    poke2Radio.value = poke2.id;
+    poke2Radio.value = poke2.pokemon;
     poke2Radio.checked = false;
-    poke2Image.src = `./assets/${poke2.image}`;
+    poke2Image.src = poke2.url_image;
     
-    poke3Radio.value = poke3.id;
+    poke3Radio.value = poke3.pokemon;
     poke3Radio.checked = false;
-    poke3Image.src = `./assets/${poke3.image}`;
+    poke3Image.src = poke3.url_image;
 }
 
-encounterPokemon();
+encounterPokemon(pokemon);
 
 capturePokemon.addEventListener('click', () => {
     if (totalPlays < 2) {
--- test/storage-utils.test.js
+++ test/storage-utils.test.js
@@ -1,18 +1,123 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { setPokedex, getPokedex } from '../storage-utils.js';
+// import { encounterPokemon } from '../app.js';
+import pokemon from '../data/pokemon.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+test('setPokedex should create results object if pokemon not previously shown', (expect) => {
+    localStorage.removeItem('RESULTS');
+    const fakeResult = {
+        id: 1,
+        shown: 1,
+        preferred: 0
+    };
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    setPokedex(1);
+
+    const resultsString = localStorage.getItem('RESULTS') || '[]';
+    const results = JSON.parse(resultsString);
+
+    expect.deepEqual(results[0], fakeResult);
 });
+
+test('setPokedex should increment object', (expect) => {
+    localStorage.removeItem('RESULTS');
+    const fakeResult = [{
+        id: 1,
+        shown: 1,
+        preferred: 0
+    }];
+    localStorage.setItem('RESULTS', JSON.stringify(fakeResult));
+
+    setPokedex(1);
+
+    const resultsString = localStorage.getItem('RESULTS') || '[]';
+    const results = JSON.parse(resultsString);
+
+    const expected = {
+        id: 1,
+        shown: 2,
+        preferred: 0
+    };
+    expect.deepEqual(results[0], expected);
+});
+
+test('getPokedex should ', expect => {
+    const expected = {
+        '_id':'5cef3501ef6005a77cd4fd17',
+        'pokemon':'bulbasaur',
+        'id':1,
+        'species_id':1,
+        'height':7,
+        'weight':69,
+        'base_experience':64,
+        'type_1':'grass',
+        'type_2':'poison',
+        'attack':49,
+        'defense':49,
+        'hp':45,
+        'special_attack':65,
+        'special_defense':65,
+        'speed':45,
+        'ability_1':'overgrow',
+        'ability_2':'NA',
+        'ability_hidden':'chlorophyll',
+        'color_1':'#78C850',
+        'color_2':'#A040A0',
+        'color_f':'#81A763',
+        'egg_group_1':'monster',
+        'egg_group_2':'plant',
+        'url_image':'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        'generation_id':1,
+        'evolves_from_species_id':'NA',
+        'evolution_chain_id':1,
+        'shape_id':8,
+        'shape':'quadruped',
+        'pokebase':'bulbasaur',
+        'pokedex':'http://www.pokemon.com/us/pokedex/bulbasaur'
+    };
+    const actual = getPokedex(pokemon, 1);
+    expect.deepEqual(actual, expected);
+});
+
+// test('encounterPokemon should display the pokemon whose id correlates with the random number generated', expect =>{
+//     const num = [{
+//         '_id':'5cef3501ef6005a77cd4fd17',
+//         'pokemon':'bulbasaur',
+//         'id':1,
+//         'species_id':1,
+//         'height':7,
+//         'weight':69,
+//         'base_experience':64,
+//         'type_1':'grass',
+//         'type_2':'poison',
+//         'attack':49,
+//         'defense':49,
+//         'hp':45,
+//         'special_attack':65,
+//         'special_defense':65,
+//         'speed':45,
+//         'ability_1':'overgrow',
+//         'ability_2':'NA',
+//         'ability_hidden':'chlorophyll',
+//         'color_1':'#78C850',
+//         'color_2':'#A040A0',
+//         'color_f':'#81A763',
+//         'egg_group_1':'monster',
+//         'egg_group_2':'plant',
+//         'url_image':'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+//         'generation_id':1,
+//         'evolves_from_species_id':'NA',
+//         'evolution_chain_id':1,
+//         'shape_id':8,
+//         'shape':'quadruped',
+//         'pokebase':'bulbasaur',
+//         'pokedex':'http://www.pokemon.com/us/pokedex/bulbasaur'
+//     }];
+//     const expected = `<label>
+//     <input id="poke1-radio" name="pokemon" type="radio" value="charmander">
+//     <img id="poke1-image" src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png">
+//     </label>`;
+//     const actual = encounterPokemon(num);
+//     expect.equal(actual, expected);
+// });
